795afe9c7f6a8ff61891b6e9b62d35b177cf2489
Amr Khalid <Kamr80001@gmail.com>
2026-07-28 23:20:31 +0300

    refactor(utils): optimize dateHelpers timezone formatting and relative time labels

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -81,3 +81,4 @@ export function formatDayNumber(date: Date): string {
 }
 
 export { isToday, isSameDay, subDays, addDays, parseISO, differenceInDays, startOfDay, format };
+ // Timezone relative format
